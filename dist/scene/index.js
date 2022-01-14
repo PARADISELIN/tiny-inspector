23f8f9e9010d6db5d1e1125425808bd2d65af1f7
linlei <lin92731@outlook.com>
2022-01-14 14:06:19 +0800

    feat: extend scene script

---
+++ dist/scene/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.paths.push(require('path').join(Editor.App.path, 'node_modules'));
+
+exports.load = function () {};
+
+exports.unload = function () {};
+
+exports.methods = {
+  setNodePosition(uuid, position) {
+    const node = globalThis.cce.Node.query(uuid);
+    node.setPosition(position.x, position.y, position.z);
+  }
+
+};
---
+++ src/scene/index.ts
@@ -0,0 +1,14 @@
+import type { Position } from '../panels/default/script/components/PositionProp'
+
+module.paths.push(require('path').join(Editor.App.path, 'node_modules'))
+
+exports.load = function () {}
+
+exports.unload = function () {}
+
+exports.methods = {
+  setNodePosition(uuid: string, position: Position) {
+    const node = (globalThis as any).cce.Node.query(uuid)
+    node.setPosition(position.x, position.y, position.z)
+  }
+}
